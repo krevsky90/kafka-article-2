e6387d226289d0c11e86a30b247e25b3603a6b13
krevsky90 <krevskiigrigorii@gmail.com>
2026-04-28 16:57:25 +0300

    info: https://chatgpt.com/g/g-p-69de2569c3f481918b01d49dddd12f4c/c/69ed2010-6148-83eb-ac62-6a4cf4681d7e https://chatgpt.com/g/g-p-69de2569c3f481918b01d49dddd12f4c/c/69f092ce-c6ec-83eb-96b1-d57e05c7c18c set 8080 port to all services in docker add actuator to all services (except krev-service and dlt-processor-service) add prometheus configuration to scrape metrics of all services (except krev-service and dlt-processor-service) add grafana and dashboards to visualize metrics of the services

--- krevsky_readme.txt
+++ krevsky_readme.txt
@@ -3,3 +3,9 @@ info: https://habr.com/ru/articles/961048/
 todo:
 1) add flyway instead of running kafka-article-2-krev\inventory-service\src\main\resources\inventory.sql manually
 2) add grafana to monitor kafka resources, offset lags etc
+? 3) прикрутить Node Exporter Full dashboard к order-service
+4) выяснить, какие метрики обычно требуются для спрингбут приложений, какие под это уже есть дашборды, и какие вывод можно сделать из этих метрик
+5) добавить панель с моей кастомной метрикой-counter-ом
+6) симитировать нагрузочное тестирование
+7) научиться писать запросы к прометеусу
+    типа label_values(jvm_memory_used_bytes, instance)
--- order-service/src/main/java/io/mitochondria/order/service/OrderService.java
+++ order-service/src/main/java/io/mitochondria/order/service/OrderService.java
@@ -1,18 +1,25 @@
 package io.mitochondria.order.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.mitochondria.events.order.OrderPlacedEvent;
 import io.mitochondria.order.dto.OrderRequest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import io.micrometer.core.instrument.Counter;
 
 import java.util.UUID;
 
 @Service
 public class OrderService {
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+    //custom metric (for grafana)
+    private final Counter sentMessagesCounter;
 
-    public OrderService(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate) {
+    public OrderService(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate, MeterRegistry registry) {
         this.kafkaTemplate = kafkaTemplate;
+        this.sentMessagesCounter = Counter.builder("orderService.messages.sent")
+                .description("Total messages sent to kafka")
+                .register(registry);
     }
 
     public String placeOrder(OrderRequest orderRequest) {
@@ -25,6 +32,8 @@ public class OrderService {
         );
 
         kafkaTemplate.send("order-placed", orderPlacedEvent.getOrderId().toString(), orderPlacedEvent);
+
+        sentMessagesCounter.increment();    //increase metric
 
         return orderId;
     }
