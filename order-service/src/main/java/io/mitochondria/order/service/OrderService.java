bcb00fa08da3cddfae8e170314c2b1e9181b62fc
krevsky90 <krevskiigrigorii@gmail.com>
2026-05-15 10:12:27 +0300

    info: https://chatgpt.com/g/g-p-69de2569c3f481918b01d49dddd12f4c/c/6a035b5a-bf6c-83eb-b8de-39c2c2594d7c
    add business custom metrics to order-service
    limit jaeger container to avoid memory extension
    add examples of popular PromQL queries

--- order-service/src/main/java/io/mitochondria/order/service/OrderService.java
+++ order-service/src/main/java/io/mitochondria/order/service/OrderService.java
@@ -1,6 +1,8 @@
 package io.mitochondria.order.service;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import io.mitochondria.events.order.OrderPlacedEvent;
 import io.mitochondria.order.dto.OrderRequest;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,32 +14,70 @@ import java.util.UUID;
 @Service
 public class OrderService {
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
-    //custom metric (for grafana)
+    private final MeterRegistry meterRegistry;
+    //custom metrics (for grafana)
     // will be shown in prometheus like:
     // # HELP orderService_messages_sent_total Total messages sent to kafka
     // # TYPE orderService_messages_sent_total counter
     // orderService_messages_sent_total{application="order-service"} 1.0
     private final Counter sentMessagesCounter;
+    private final Counter failedOrdersCounter;
+    // latency publish + ack from Kafka
+    private final Timer kafkaPublishTimer;
 
-    public OrderService(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate, MeterRegistry registry) {
+    public OrderService(KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate, MeterRegistry meterRegistry) {
         this.kafkaTemplate = kafkaTemplate;
-        this.sentMessagesCounter = Counter.builder("orderService.messages.sent")
-                .description("Total messages sent to kafka")
-                .register(registry);
+        this.meterRegistry = meterRegistry;
+
+        this.sentMessagesCounter =
+                Counter.builder("order.messages.sent")
+                        .description("Total successfully published kafka messages")
+                        .register(meterRegistry);
+
+        this.failedOrdersCounter =
+                Counter.builder("order.create.failures")
+                        .description("Total failed order creations")
+                        .register(meterRegistry);
+
+        this.kafkaPublishTimer =
+                Timer.builder("order.kafka.publish.duration")
+                        .description("Kafka publish acknowledgement duration")
+                        .publishPercentileHistogram()
+                        .publishPercentiles(0.5, 0.95, 0.99)
+                        .register(meterRegistry);
+
     }
 
+    //NOTE: we can use Spring AOP to add custom metric (but only for the whole method)
+    @Timed(
+            value = "order.create.duration",
+            description = "Order creation duration",
+            histogram = true,
+            percentiles = {0.5, 0.95, 0.99}
+    )
     public String placeOrder(OrderRequest orderRequest) {
         String orderId = UUID.randomUUID().toString();
         OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(
-            orderId,
-            orderRequest.email(),
-            orderRequest.productName(),
-            orderRequest.quantity()
+                orderId,
+                orderRequest.email(),
+                orderRequest.productName(),
+                orderRequest.quantity()
         );
 
-        kafkaTemplate.send("order-placed", orderPlacedEvent.getOrderId().toString(), orderPlacedEvent);
+        Timer.Sample sample = Timer.start(meterRegistry);
 
-        sentMessagesCounter.increment();    //increase metric
+        //NOTE: use whenComplete to get the result once publishing is done
+        kafkaTemplate
+                .send("order-placed", orderPlacedEvent.getOrderId().toString(), orderPlacedEvent)
+                .whenComplete((result, ex) -> {
+                    sample.stop(kafkaPublishTimer);
+                    if (ex == null) {
+                        sentMessagesCounter.increment();    //increase metric
+                    } else {
+                        failedOrdersCounter.increment();
+                    }
+                });
+
 
         return orderId;
     }
